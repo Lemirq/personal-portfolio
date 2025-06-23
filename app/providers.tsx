b078c50bccb64e3d2c9edfd223751111e064f8a4
Vihaan Sharma <86086039+Lemirq@users.noreply.github.com>
2025-06-23 12:54:31 -0400

    feat: add PostHog integration

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,11 @@
-// app/providers.tsx
 'use client';
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
+import { PostHogProvider } from '@/components/PostHogProvider';
 
 import { MainStoreProvider } from '@/stores/main-state-provider';
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-  });
-}
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <PostHogProvider client={posthog}>
+    <PostHogProvider>
       <MainStoreProvider>{children}</MainStoreProvider>
     </PostHogProvider>
   );
